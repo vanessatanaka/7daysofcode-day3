8eff06dfba57d362d9f379d68bd2cc82c5585966
Vanessa Tanaka <vanessatanaka@live.com>
2025-02-28 14:52:17 -0300

    parte 2 (carreira, com melhorias do ChatGPT e ajustes pessoais 2)

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // 7Daysofcode - Dia 3
 
+// Variáveis
+let linguagem = "";
+let carreira = "";
+
 // Função para exibir mensagens de alerta
 function mostrarMensagem(mensagem) {
   alert(mensagem);
@@ -14,13 +18,16 @@ function perguntar(mensagem, opcoesValidas) {
   return resposta;
 }
 
+// Objeto com opções de mensagem de acordo com a Carreira escolhida
+const mensagensCarreira = {
+  "1": "Excelente decisão!\n O especialista foca em uma área específica do desenvolvimento, adquirindo um conhecimento profundo.\n Pode ser especialista em Back-End, Front-End, Banco de Dados, Arquitetura de Software, Segurança da Informação, entre outras.\n Normalmente, é a referência técnica para problemas mais complexos e otimizações avançadas.",
+  "2": "Excelente decisão!\n O desenvolvedor fullstack atua tanto no Back-End quanto no Front-End, podendo também ter conhecimentos em banco de dados, infraestrutura e DevOps.\n Possui um conhecimento amplo sobre várias tecnologias e ferramentas, conseguindo desenvolver um sistema completo de ponta a ponta."
+}
+
 // Escolha da área
-const area = perguntar(
-  "Qual área deseja seguir?\n 1 Front-end\n 2 Back-end.", ["1", "2"]);
+const area = perguntar("Qual área deseja seguir?\n 1 Front-end\n 2 Back-end.", ["1", "2"]);
 
 // Escolha da linguagem + Escolha da carreira
-let linguagem;
-let carreira;
 if (area === "1") {
   linguagem = perguntar("Front-end é uma boa escolha!\n O que você deseja aprender?\n 1 React\n 2 Vue.", ["1", "2"]);
   carreira = perguntar("Você pretende ser um especialista em Front-end ou um desenvolvedor fullstack?\n 1 Especialista\n 2 Fullstack\n 3 Quero saber a diferença.", ["1", "2", "3"]);
@@ -39,10 +46,5 @@ if (carreira === "3") {
   carreira = perguntar("Agora que você sabe a diferença, o que pretende ser?\n 1 Especialista\n 2 Fullstack.", ["1", "2"]);
 }
 
-// Mensagem final de acordo com a escolha
-const mensagensCarreira = {
-  "1": "Excelente decisão!\n O especialista foca em uma área específica do desenvolvimento, adquirindo um conhecimento profundo.\n Pode ser especialista em Back-End, Front-End, Banco de Dados, Arquitetura de Software, Segurança da Informação, entre outras.\n Normalmente, é a referência técnica para problemas mais complexos e otimizações avançadas.",
-  "2": "Excelente decisão!\n O desenvolvedor fullstack atua tanto no Back-End quanto no Front-End, podendo também ter conhecimentos em banco de dados, infraestrutura e DevOps.\n Possui um conhecimento amplo sobre várias tecnologias e ferramentas, conseguindo desenvolver um sistema completo de ponta a ponta."
-}
-
+// Mensagem final de acordo com a Carreira escolhida
 mostrarMensagem(mensagensCarreira[carreira]);
